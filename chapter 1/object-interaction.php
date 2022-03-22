34b8faeef85f5cc998ca4ca99298f864289aab18
OBrien-reece <indeche.evans.o@gmail.com>
2022-03-23 02:22:40 +0300

    Object Interaction

---
+++ chapter 1/object-interaction.php
@@ -0,0 +1,51 @@
+<?php
+class Song {
+    public $songId;
+    public $songTitle;
+}
+
+class Playlist {
+    public $name;
+    public $songs = [];
+
+    public function addSong($song) {
+        $this->songs[] = $song;
+    }
+}
+
+$octopusGarden = new Song();
+$octopusGarden->songId = 1;
+$octopusGarden->songTitle = "Octopus Garden";
+// var_dump($octopusGarden);
+
+$playlist = new Playlist();
+$playlist->name = "Rock";
+$playlist->addSong($octopusGarden);
+// var_dump($playlist->songs);
+// print_r($playlist->songs);
+
+
+class Sing {
+    public $sing;
+    public $artist;
+}
+
+class MyPlaylist {
+    public $name = "2.b.d lited";
+    public $songs = [];
+
+    public function addSong($sing) {
+        $this->songs = $sing;
+    }
+}
+
+$theSing = new Sing();
+$theSing->sing = "Is this the life I chose";
+$theSing->artist = "Aryan";
+// var_dump($theSing);
+
+$myPlaylist = new MyPlaylist();
+$myPlaylist->name;
+$myPlaylist->songs;
+$myPlaylist->addSong($theSing);
+var_dump($myPlaylist->songs);
